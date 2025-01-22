7c3d744c12b21223ec89433c610260e873e786de
Phyo Than Htike <6531503173@lamduan.mfu.ac.th>
2025-01-22 14:44:03 +0700

    fix

--- Backend/Controllers/courses.js
+++ Backend/Controllers/courses.js
@@ -1,24 +1,13 @@
-<<<<<<< HEAD
 const { eq, and } = require("drizzle-orm");
-const cloudinary = require("cloudinary").v2;
-const db = require("../db/db");
-const { allcourses, modules, lessons } = require("../db");
 const {
   courseSchema,
   moduleSchema,
   lessonSchema,
 } = require("../types/EduSchema");
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
 
-const { eq } = require("drizzle-orm");
 const cloudinary = require("../Action/cloudinary");
 const db = require("../db/db");
 const { allcourses, modules, lessons } = require("../db");
-const { courseSchema, moduleSchema } = require("../types/EduSchema");
 
 
 exports.getAllCourses = async (req, res) => {
