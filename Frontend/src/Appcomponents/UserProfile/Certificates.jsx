820b80333bcf4061c9eea25e3c4b5d56068c27d5
Phyo Than Htike <6531503173@lamduan.mfu.ac.th>
2025-04-18 20:57:36 +0700

    certificate table

--- Frontend/src/Appcomponents/UserProfile/Certificates.jsx
+++ Frontend/src/Appcomponents/UserProfile/Certificates.jsx
@@ -1,7 +1,15 @@
-import { GetCertificate } from "@/EndPoints/user";
 import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { useTranslation } from "react-i18next";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 const Certificates = ({ certificate }) => {
   const { t } = useTranslation();
@@ -11,32 +19,46 @@ const Certificates = ({ certificate }) => {
   });
   return (
     <>
-      <h1 className="text-[18px] text-center pt-4">{certificates}</h1>
+      <h1 className="text-[18px] text-center pt-8 font-bold">{certificates}</h1>
       <div className="bg-pale rounded-xl h-auto w-full my-7 overflow-y-auto">
         {certificate.length > 0 ? (
-          <div className="grid grid-cols-3 gap-4 p-4">
-            {certificate.map((item, index) => (
-              <div
-                key={index}
-                className="w-full flex flex-col items-center justify-center text-center gap-4 h-[200px] rounded-xl bg-white p-4"
-              >
-                <div className="h-[180px] flex items-center text-center">
-                  <p className="font-semibold text-base">
-                    {item.course_name || "Course Name Missing"}
-                  </p>
+          <>
+            <Table className="mt-4">
+              <TableHeader className="text-base">
+                <TableRow>
+                  <TableHead className="text-center">Course Name</TableHead>
+                  <TableHead className="text-center">Action</TableHead>
+                </TableRow>
+              </TableHeader>
+              {certificate ? (
+                certificate.map((item, index) => (
+                  <TableBody key={index}>
+                    <TableRow>
+                      <TableCell className="font-medium text-center">
+                        {item.course_name}
+                      </TableCell>
+                      <TableCell className="flex justify-center items-center">
+                        <a
+                          href={item.certificate_url}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="w-full flex justify-center"
+                        >
+                          <Button className="w-[50%] flex justify-center items-center">
+                            {view}
+                          </Button>
+                        </a>
+                      </TableCell>
+                    </TableRow>
+                  </TableBody>
+                ))
+              ) : (
+                <div>
+                  <p>No Certificates to show.</p>
                 </div>
-
-                <a
-                  href={item.certificate_url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="text-white w-full"
-                >
-                  <Button className="w-full">{view}</Button>
-                </a>
-              </div>
-            ))}
-          </div>
+              )}
+            </Table>
+          </>
         ) : (
           <div className="text-center py-12 text-gray-400">{nothing}</div>
         )}
@@ -46,3 +68,29 @@ const Certificates = ({ certificate }) => {
 };
 
 export default Certificates;
+
+{
+  /* <div className="grid grid-cols-3 gap-4 p-4">
+{certificate.map((item, index) => (
+  <div
+    key={index}
+    className="w-full flex flex-col items-center justify-center text-center gap-4 h-[200px] rounded-xl bg-white p-4"
+  >
+    <div className="h-[180px] flex items-center text-center">
+      <p className="font-semibold text-base">
+        {item.course_name || "Course Name Missing"}
+      </p>
+    </div>
+
+    <a
+      href={item.certificate_url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-white w-full"
+    >
+      <Button className="w-full">{view}</Button>
+    </a>
+  </div>
+))}
+</div> */
+}
